19cb82509d27dcc8c5cd8cace584c71cb58146a9
Débora <106615632+deborasza@users.noreply.github.com>
2022-12-30 21:44:01 -0300

    Update testeDAO.py

---
+++ Sistema/Implementação/testeDAO.py
@@ -0,0 +1,12 @@
+from pedidosBean import PedidosBean
+from pedidosDAO import PedidosDAO
+
+#Pedido no Banco de Dados
+p = PedidosBean()
+p.setCliente('Débora')
+p.setPedido('Doritos')
+p.setQuantidade(4)
+p.setValor(8)
+
+pedidos_dao = PedidosDAO()
+pedidos_dao.Inserir_Pedido(p)
