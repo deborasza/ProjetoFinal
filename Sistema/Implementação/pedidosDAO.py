ff26e9e4688c4ab02b0d6cba9abfeb6724d7c1f4
Débora <106615632+deborasza@users.noreply.github.com>
2022-12-30 21:40:45 -0300

    Update pedidosDAO.py

---
+++ Sistema/Implementação/pedidosDAO.py
@@ -0,0 +1,15 @@
+#DAO (Data Access Object)
+
+import psycopg2
+
+class PedidosDAO:
+
+    def __init__(self):
+        self.conexao = psycopg2.connect(database = "PDS", user = "postgres", password = "kluyvert", host = "localhost", port = "5432")
+        self.cursor = self.conexao.cursor()
+    
+    #Método para inserir o Pedido no Banco de Dados:
+    def Inserir_Pedido(self, pedidos):
+        sql = f"insert into pedidos values ('{pedidos.getCliente()}', '{pedidos.getPedido()}' ,{pedidos.getQuantidade()}, {pedidos.getValor()})"
+        self.cursor.execute(sql)
+        self.conexao.commit()
