ac6fe437f104e21632b4fa3519bbff0b7a64981b
Débora <deborasouza.sb@gmail.com>
2023-01-03 18:27:09 -0300

    gerar lista para motorista

--- Sistema/Implementação/pedidos.py
+++ Sistema/Implementação/pedidos.py
@@ -246,6 +246,121 @@ def Gerar_Relatorio():
     except:
         messagebox.showerror('Erro!', 'Não foi possível gerar Relatório.')
 
+###########################################################################################################
+def Gerar_Lista_Motorista():
+    try:
+        #Endereço onde o arquivo será criado + nome do arquivo
+        end_arq = "C:\\Users\\55849\\Documents\\ProjetoFinal\\Sistema\\Relatório\\relatorio_pedidos.pdf"
+        
+        #Criação do arquivo pdf:
+        pdf = canvas.Canvas(end_arq, pagesize = A4)
+        #Metadados sobre o arquivo:
+        pdf.setTitle('Relatório de Pedidos')
+        pdf.setAuthor('Débora, Deivid, Giovanna e Isabel')
+        pdf.setKeywords('Queijo, CT, Sistema, Python, Cadastro, Pedidos')
+        
+        ##Escrevendo o conteúdo do arquivo pdf##
+        #Definição da fonte e do tamanho da letra:
+        pdf.setFont('Times-Bold', 20)
+        #Definição de um título para o arquivo pdf:
+        pdf.drawCentredString(width/2, 750, "Relatório de Pedidos")
+        
+        #Definição de um subtítulo para o arquivo pdf:
+        pdf.setFont('Times-Roman', 15)
+        pdf.setFillColor(colors.brown) #Definição da cor da fonte
+        pdf.drawCentredString(width/2, 725, "Centro de Tecnologia do Queijo - Campus Currais Novos")
+        
+        #Desenhando uma linha horizontal:
+        pdf.line(30, 705, 550, 705) #Coordenadas iniciais e finais da linha
+
+        ##Inserção de um parágrafo:##
+        #Escrevendo o texto:
+        paragrafo = ['Tabela de pedidos realizados e suas devidas informações:'] #As informações dentro das aspas, representa uma linha do arquivo pdf.
+
+        #Definindo onde esse texto irá começar:
+        text = pdf.beginText(40, 685)
+
+        #Definindo o tipo da fonte:
+        text.setFont('Times-Roman', 12)
+        text.setFillColor(colors.black) #Definindo a cor da fonte
+        
+        #Adicionando uma lista de itens:
+        text.textLines(paragrafo)
+        pdf.drawText(text)
+
+        #Lista de valores:
+        dados = [
+            ['Nome do Cliente', 'Pedido', 'Quantidade', 'Valor Unitário', 'Valor Total', 'Rua', 'Nº da Casa', 'Bairro', 'CEP'],
+        ]
+
+        #Definição da consulta SQL que retorna todos os dados da tabela 'animes':
+        sql = 'select * from pedidos'
+
+        cursor.execute(sql)
+        resultado = cursor.fetchall() #Lista de itens que estão no Banco de Dados
+        #print(resultado)
+        for linha in resultado:
+            #print("-------------------------------------------------------------------------------------------------------------------------------------")
+            cli = str(linha[0]) #Coluna onde está o nome do cliente
+            p = str(linha[1]) #Coluna onde está o pedido
+            quant = str(linha[2]) #Coluna onde está a quantidade
+            vu = str(linha[3]) #Coluna onde está o valor
+            vt = str(linha[4]) #Coluna onde está o valor total
+            rua = str(linha[5]) #Coluna onde está a rua
+            n_casa = str(linha[6]) #Coluna onde está o número da casa
+            bairro = str(linha[7]) #Coluna onde está o bairro 
+            cep = str(linha[8]) #Coluna onde está o CEP
+            #Item que contém os dados que estão vindo do Banco de Dados:
+            item = [cli, p, quant, vu, vt, rua, n_casa, bairro, cep]
+            #Adição do item ao conjunto de dados:
+            dados.append(item)
+
+        #Criação da tabela:
+        tabela = Table(dados)
+
+        #Montando item por item
+        estilo = TableStyle([
+            #Cor de fundo da primeira linha:
+            ('BACKGROUND', (0,0), (6,0), colors.brown), 
+            #Cor do texto da primeira linha:
+            ('TEXTCOLOR', (0,0), (6,0), colors.whitesmoke), 
+            #Fonte da tabela inteira:
+            ('FONTNAME', (0,0), (6,-1), 'Courier-Bold'), #-1 representa o último elemento
+            #Tamanho da fonte:
+            ('FONTSIZE', (0,0), (6,-1), 8.5),
+            #Alinhamento do texto:
+            ('ALIGN', (0,0), (6,-1), 'CENTER'),
+            #Cor de fundo dos dados:
+            ('BACKGROUND', (0,1), (6,-1), colors.beige),
+            #Cor do texto dos dados:
+            ('TEXTCOLOR', (0,1), (6,-1), colors.black),
+            #Tamanho e cor da borda:
+            ('GRID', (0,0), (6,-1), 0.5, colors.brown)
+        ])
+        #Adicionar estilo à tabela:
+        tabela.setStyle(estilo)
+
+        #Tamanho da tabela:
+        tabela.wrapOn(pdf, 400, 100)
+        #Posicionamento da tabela:
+        tabela.drawOn(pdf, 30, 585) 
+
+        #Inserir uma imagem:
+        imagem1 = "C:\\Users\\55849\\Documents\\ProjetoFinal\\Sistema\\Imagens\\1.png"
+        imagem2 = "C:\\Users\\55849\\Documents\\ProjetoFinal\\Sistema\\Imagens\\2.jpg"
+        imagem3 = "C:\\Users\\55849\\Documents\\ProjetoFinal\\Sistema\\Imagens\\3.jpeg"
+        pdf.drawInlineImage(imagem1, 240, 780)
+        pdf.drawInlineImage(imagem2, width/2, 790)
+        pdf.drawInlineImage(imagem3, 330, 780)
+
+        #Salvando o arquivo:
+        pdf.save()
+
+        messagebox.showinfo('Sucesso!', 'Relatório gerado com sucesso.')
+    except:
+        messagebox.showerror('Erro!', 'Não foi possível gerar Relatório.')
+###########################################################################################################
+
 janela = Tk() #Janela recebe uma instância de Tk()
 
 #Gerenciador de Abas
@@ -270,22 +385,46 @@ aba1_PedidoEntry = Entry(aba1, width = 30)
 aba1_PedidoEntry.place(x = 400, y = 100)
 
 aba1_QuantidadeLabel = Label(aba1, text = "Quantidade:")
-aba1_QuantidadeLabel.place(x = 100 , y = 180)
+aba1_QuantidadeLabel.place(x = 100 , y = 140)
 
 aba1_QuantidadeEntry = Entry(aba1, width = 30)
-aba1_QuantidadeEntry.place(x = 100, y = 200)
+aba1_QuantidadeEntry.place(x = 100, y = 160)
 
 aba1_ValorLabel = Label(aba1, text = "Valor Unitário:")
-aba1_ValorLabel.place(x = 400 , y = 180)
+aba1_ValorLabel.place(x = 400 , y = 140)
 
 aba1_ValorEntry = Entry(aba1, width = 30)
-aba1_ValorEntry.place(x = 400 , y = 200)
+aba1_ValorEntry.place(x = 400 , y = 160)
+
+aba1_RuaLabel = Label(aba1, text = "Rua para Entrega:")
+aba1_RuaLabel.place(x = 100 , y = 200)
+
+aba1_RuaEntry = Entry(aba1, width = 30)
+aba1_RuaEntry.place(x = 100 , y = 220)
+
+aba1_NumeroCasaLabel = Label(aba1, text = "Nº da Casa para Entrega:")
+aba1_NumeroCasaLabel.place(x = 400 , y = 200)
+
+aba1_NumeroCasaEntry = Entry(aba1, width = 30)
+aba1_NumeroCasaEntry.place(x = 400 , y = 220)
+
+aba1_BairroLabel = Label(aba1, text = "Bairro para Entrega:")
+aba1_BairroLabel.place(x = 100 , y = 260)
+
+aba1_BairroEntry = Entry(aba1, width = 30)
+aba1_BairroEntry.place(x = 100 , y = 280)
+
+aba1_CEPLabel = Label(aba1, text = "CEP para Entrega:")
+aba1_CEPLabel.place(x = 400 , y = 260)
+
+aba1_CEPEntry = Entry(aba1, width = 30)
+aba1_CEPEntry.place(x = 400 , y = 280)
 
 aba1_CadastrarButton = Button(aba1, width = 26, text = "Cadastrar Pedido", command = Inserir_Dados)
-aba1_CadastrarButton.place(x = 100 , y = 300)
+aba1_CadastrarButton.place(x = 100 , y = 340)
 
 aba1_LimparDados = Button(aba1, width = 26, text = "Limpar Dados", command = Limpar_Dados_Aba1)
-aba1_LimparDados.place(x = 400, y = 300)
+aba1_LimparDados.place(x = 400, y = 340)
 
 #Aba 2 - Visualizar Dados:
 aba2 = ttk.Frame(gerenciadorAba)
@@ -311,7 +450,7 @@ gerenciadorAba.add(aba3, text = "Atualizar Dados")
 gerenciadorAba.pack(expand = 1, fill = "both")
 
 ## Elementos da Aba 3 ###
-aba3_ClienteLabel = Label(aba3, text = "Nome do Cliente:")
+aba3_ClienteLabel = Label(aba3, text = "Nome Completo do Cliente:")
 aba3_ClienteLabel.place(x = 100, y = 80)
 
 aba3_ClienteEntry = Entry(aba3, width = 30)
@@ -323,23 +462,47 @@ aba3_NovoPedidoLabel.place(x = 400, y = 80)
 aba3_NovoPedidoEntry = Entry(aba3, width = 30)
 aba3_NovoPedidoEntry.place(x = 400, y = 100)
 
-aba3_NovaQuantidadeLabel = Label(aba3, text = "Nova quantidade:")
-aba3_NovaQuantidadeLabel.place(x = 100, y = 180)
+aba3_NovaQuantidadeLabel = Label(aba3, text = "Nova Quantidade:")
+aba3_NovaQuantidadeLabel.place(x = 100, y = 140)
 
 aba3_NovaQuantidadeEntry = Entry(aba3, width = 30)
-aba3_NovaQuantidadeEntry.place(x = 100, y = 200)
+aba3_NovaQuantidadeEntry.place(x = 100, y = 160)
 
 aba3_NovoValorLabel = Label(aba3, text = "Novo Valor Unitário:")
-aba3_NovoValorLabel.place(x = 400 , y = 180)
+aba3_NovoValorLabel.place(x = 400 , y = 140)
 
 aba3_NovoValorEntry = Entry(aba3, width = 30)
-aba3_NovoValorEntry.place(x = 400, y = 200)
+aba3_NovoValorEntry.place(x = 400, y = 160)
+
+aba3_NovaRuaLabel = Label(aba3, text = "Nova Rua para Entrega:")
+aba3_NovaRuaLabel.place(x = 100 , y = 200)
+
+aba3_NovaRuaEntry = Entry(aba3, width = 30)
+aba3_NovaRuaEntry.place(x = 100, y = 220)
+
+aba3_NovoNumeroLabel = Label(aba3, text = "Novo Nº da Casa para Entrega:")
+aba3_NovoNumeroLabel.place(x = 400 , y = 200)
+
+aba3_NovoNumeroEntry = Entry(aba3, width = 30)
+aba3_NovoNumeroEntry.place(x = 400, y = 220)
+
+aba3_NovoBairroLabel = Label(aba3, text = "Novo Bairro para Entrega:")
+aba3_NovoBairroLabel.place(x = 100 , y = 260)
+
+aba3_NovoBairroEntry = Entry(aba3, width = 30)
+aba3_NovoBairroEntry.place(x = 100, y = 280)
+
+aba3_NovoCEPLabel = Label(aba3, text = "Novo CEP para Entrega:")
+aba3_NovoCEPLabel.place(x = 400 , y = 260)
+
+aba3_NovoCEPEntry = Entry(aba3, width = 30)
+aba3_NovoCEPEntry.place(x = 400, y = 280)
 
 aba3_AtualizarButton = Button(aba3, width = 25, text = "Atualizar Dados", command = Atualizar_Dados)
-aba3_AtualizarButton.place(x = 100, y = 300)
+aba3_AtualizarButton.place(x = 100, y = 340)
 
 aba3_LimparDadosButton = Button(aba3, width = 25, text = "Limpar Dados", command = Limpar_Dados_Aba3)
-aba3_LimparDadosButton.place(x = 400, y = 300)
+aba3_LimparDadosButton.place(x = 400, y = 340)
 
 #Aba 4 - Remover Dados:
 aba4 = ttk.Frame(gerenciadorAba)
